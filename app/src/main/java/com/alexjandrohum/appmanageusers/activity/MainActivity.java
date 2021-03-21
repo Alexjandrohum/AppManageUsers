78c8e9a400ad299ce1fe8c855ea0bafcb314f77d
Alexjandrohum <alexjandrohum@MiMac.local>
2021-03-20 21:14:57 -0600

    Estructuracion del proyecto

--- app/src/main/java/com/alexjandrohum/appmanageusers/activity/MainActivity.java
+++ app/src/main/java/com/alexjandrohum/appmanageusers/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.alexjandrohum.appmanageusers;
+package com.alexjandrohum.appmanageusers.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.alexjandrohum.appmanageusers.R;
 
 public class MainActivity extends AppCompatActivity {
 
