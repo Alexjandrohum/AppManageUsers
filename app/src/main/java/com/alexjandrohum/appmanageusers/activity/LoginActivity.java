c9e85f383480721811d40967b8371af6cfd3c084
Alexjandrohum <alexjandrohum@MiMac.local>
2021-03-28 02:08:42 -0600

    Creacion de la activity Login - diseño

--- app/src/main/java/com/alexjandrohum/appmanageusers/activity/LoginActivity.java
+++ app/src/main/java/com/alexjandrohum/appmanageusers/activity/LoginActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 
 import com.alexjandrohum.appmanageusers.R;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_main);
+
+        getSupportActionBar().hide();
     }
 }
